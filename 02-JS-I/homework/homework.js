1abf10cfbe0b8f2bae6409604f84f94aa212ef85
unknown <rapuentes@uan.edu.co>
2021-11-30 16:11:10 -0500

    Made corrections to homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -121,7 +121,7 @@ function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
   
-  x % y
+  return x % y
   
 }
 
@@ -129,7 +129,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % x ==0)
+  if(num % 2 ==0)
   {
     return true
   }
@@ -143,7 +143,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % x !==0)
+  if(num % 2 !==0)
   {
     return true
   }
@@ -170,25 +170,26 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  Math.pow(num,exponent)
+  return Math.pow(num,exponent)
 }
+
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  Math.round(num)
+  return Math.round(num)
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  Math.ceil(num)
+  return Math.ceil(num)
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  Math.random()  
+  return Math.random()  
 }
 
 function esPositivo(numero) {
@@ -197,17 +198,17 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
   
-  if(num > 0)
+  if(numero > 0)
   {
-    return num = "Es positivo"
+    return numero = "Es positivo"
   }
-  else if(num == 0)
+  else if(numero == 0)
   {
-    return num = false
+    return numero = false
   }
   else
   {
-    return num = "Es negativo"
+    return numero = "Es negativo"
   }
 }
 
@@ -215,14 +216,14 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return "str" + "!"
+  return str + "!"
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return "nombre " + "apellido"
+  return nombre+ " " +apellido
   
 }
 
@@ -230,7 +231,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  "hola " + "nombre" + "!"
+  return "Hola " + nombre + "!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -251,7 +252,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  return area = (base * altura)/2
 }
 
 
@@ -260,9 +261,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  const equivalenciaEuroADolar = 1.20
+  const indiceEuroADolar = 1.20
   var dolar= null ;
-  dolar = euro * equivalenciaEuroADolar;
+  return dolar = euro * indiceEuroADolar;
 }
 
 
@@ -272,13 +273,37 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if(letra.includes("a"))
+  {
+    return "Es vocal"
+  }
+  else if(letra.includes("e"))
+  {
+    return "Es vocal"
+  }
+  else if(letra.includes("i"))
+  {
+    return "Es vocal"
+  }
+  else if(letra.includes("o"))
+  {
+    return  "Es vocal"
+  }
+  else if(letra.includes("u"))
+  {
+    return  "Es vocal"
+  }
+  else if(letra.length !== 1)
+  {
+    return "Dato incorrecto"
+  }
+  else 
+  {
+    return "Dato incorrecto"
+  }
 }
 
 
-
-// No modificar nada debajo de esta línea
-// --------------------------------
 
 module.exports = {
   nuevaString,
